04831839a2457b7140323340edc9d92cfb8fe726
Marcos Lima <60903178+malf75@users.noreply.github.com>
2025-03-20 09:35:12 -0300

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -60,11 +60,7 @@ async def create_user(db: db_dependency,
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                                  db: db_dependency):
     try:
-        user = authenticate_user(form_data.username, form_data.password, db)
-        if not user:
-            print(user)
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                detail="Credenciais incorretas")
+        return authenticate_user(form_data.username, form_data.password, db)
         else:
             if user.primeiro_login == True:
                 return {"id":user.id}
