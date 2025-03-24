c4bf0f7a7f8b6dd337c187e3225c276acf124c9a
Marcos Lima <malfbf6@gmail.com>
2025-03-24 13:10:08 -0300

    print

--- auth/auth.py
+++ auth/auth.py
@@ -139,7 +139,8 @@ def create_access_token(email: str, user_id: int, db: db_dependency):
 @router.get("/user")
 async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = await jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        print(payload)
         email: str = payload.get("sub")
         user_id: int = payload.get("id")
         if email is None or user_id is None:
