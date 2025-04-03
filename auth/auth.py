37d12410210c5ca173df7d733677e237737a8ed5
malf75 <malfbf6@gmail.com>
2025-04-02 22:28:11 -0300

    teste

--- auth/auth.py
+++ auth/auth.py
@@ -181,9 +181,7 @@ async def token_recupera_senha(email: EmailStr, db: db_dependency):
     try:
         user = select(Usuario).where(Usuario.email == email)
         query = db.exec(user).first()
-        print(query)
-        token = base64.b64encode(email).decode("utf-8")
-        print(token)
+        token = bcrypt_context.hash(email)
         cria_token = RecuperaSenha(
             usuario_id = query.id,
             token = token
