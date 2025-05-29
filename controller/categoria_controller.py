1e48f94ac671d53cb5ca9cd4e9802f6f3416b1c6
malf75 <malfbf6@gmail.com>
2025-05-29 09:03:37 -0300

    refactor: Inclusão da validação de categoria

--- controller/categoria_controller.py
+++ controller/categoria_controller.py
@@ -1,5 +1,4 @@
 from sqlmodel import select, Session
-from sqlalchemy import or_
 from database.models import Categoria
 from fastapi import HTTPException
 from starlette import status
@@ -18,30 +17,40 @@ async def retorna_categorias(user, db: Session):
     
 async def cria_categoria(tipo, categoria, user, db: Session):
     try:
-        categoria = Categoria(
-            usuario_id=user['id'],
-            tipo_id=tipo,
-            categoria=categoria
-        )
-        db.add(categoria)
-        db.commit()
-        return JSONResponse(status_code=status.HTTP_201_CREATED, content="Categoria criada com sucesso")
+        statement = select(Categoria).where(Categoria.categoria == categoria, Categoria.usuario_id == user["id"])
+        query = db.exec(statement).first()
+        if not query:
+            categoria = Categoria(
+                usuario_id=user['id'],
+                tipo_id=tipo,
+                categoria=categoria
+            )
+            db.add(categoria)
+            db.commit()
+            return JSONResponse(status_code=status.HTTP_201_CREATED, content="Categoria criada com sucesso")
+        else:
+            return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Categoria já existente")
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao criar categoria")
     
 async def edita_categoria(id, tipo, categoria, user, db: Session):
     try:
-        query = select(Categoria).where(Categoria.id == id, Categoria.usuario_id == user['id'])
-        categoria_db = db.exec(query).first()
-        if not categoria_db:
-            raise HTTPException(status_code=404, detail="Categoria não encontrada")
-        if tipo:
-            categoria_db.tipo_id = tipo
-        if categoria:
-            categoria_db.categoria = categoria
-        db.add(categoria_db)
-        db.commit()
-        return JSONResponse(status_code=status.HTTP_200_OK, content="Categoria editada com sucesso")
+        statement = select(Categoria).where(Categoria.categoria == categoria, Categoria.usuario_id == user["id"])
+        query = db.exec(statement).first()
+        if not query:
+            query = select(Categoria).where(Categoria.id == id, Categoria.usuario_id == user['id'])
+            categoria_db = db.exec(query).first()
+            if not categoria_db:
+                raise HTTPException(status_code=404, detail="Categoria não encontrada")
+            if tipo:
+                categoria_db.tipo_id = tipo
+            if categoria:
+                categoria_db.categoria = categoria
+            db.add(categoria_db)
+            db.commit()
+            return JSONResponse(status_code=status.HTTP_200_OK, content="Categoria editada com sucesso")
+        else:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Categoria já existente")
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao editar categoria")
     
